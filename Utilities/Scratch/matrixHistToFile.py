692cce98d8d8e1a1a47618cc0ce199e4dac6c5ea
Kenneth Long <kdlong@hep.wisc.edu>
2020-02-26 14:47:52 +0100

    Update matrix script to handle pt as well

--- Utilities/Scratch/matrixHistToFile.py
+++ Utilities/Scratch/matrixHistToFile.py
@@ -1,13 +1,14 @@
 # coding: utf-8
 import ROOT
-rtfile = ROOT.TFile("yW__NNLO_QCD.root")
-hist = rtfile.Get("canvas").GetListOfPrimitives().FindObject("yW__NNLO_QCD")
-outfile = ROOT.TFile("WplusToMuNu-MATRIX.root", "recreate")
+# Times bin size (0.5) and change units to pb
+#binrescale = 0.0005
+var = "pT_W"
+binrescale = 10.*0.001
+rtfile = ROOT.TFile("%s__NNLO_QCD.root" % var)
+hist = rtfile.Get("canvas").GetListOfPrimitives().FindObject("%s__NNLO_QCD" % var)
+outfile = ROOT.TFile("WplusToMuNu-MATRIX_%s.root" % var, "recreate")
 outfile.mkdir("wpmunu_matrix")
 outfile.cd("wpmunu_matrix")
-new_hist = hist.Clone("yW_lhe_mp")
-# Times bin size (0.5) and change units to pb
-print new_hist.Integral()
-new_hist.Scale(0.0005) 
-print new_hist.Integral()
+new_hist = hist.Clone("%s_lhe_mp" % (var if var != "pT_W" else "ptW"))
+new_hist.Scale(binrescale) 
 new_hist.Write()
